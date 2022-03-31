f8b2e538e5ddfb454e8ac692629d6ef28525b00d
unknown <joao.carvalho@edu.unifil.br>
2022-03-30 22:20:41 -0300

    Atualizando código (estava dando alguns erros ainda).

--- MAIN.js
+++ MAIN.js
@@ -1,220 +1,118 @@
-let intervals = []
-let intervalsDefault = []
+var E = 2.718281828459045235360287;
+var PI = 3.14159265359;
+var casas = 6;
 
-let roots = []
-
-let order = 0
-let functionOrder = 0
-let numbers = []
-
-let expression = ""
-
-let stopCriteria = 1
-let precision = 0.1
-
-function getNumbers() {
-    numbers = []
-
-    for (let i = 0; i <= order; i++) {
-        numbers[i] = parseFloat(document.getElementById(String.fromCharCode(i + 65)).value)
+function POW(x, y){
+    var n = x;
+    for(i = 1; i < y; ++i){
+        n *= x;  
     }
+    return n;
 }
 
-function getPrecision() {
-    precision = parseFloat(document.getElementById("getPrecision").value)
+function LOG(x, base){
+    return Math.log(x) / Math.log(base);
 }
 
-function calculateIntervals() {
-    intervals = []
-    intervalsDefault = []
+function LN(x){
+    return LOG(x, E);
+}
 
-    for (let i = -6; i < 6; i++) {
-        let elevate = order
+function roundValue(value){
+    return parseFloat(value.toFixed(casas));
+}
 
-        let numA = 0
-        let numB = 0
+function exeExpressao(expressao, X){
+    return eval(expressao.trim().toUpperCase());
+}
 
-        let k = i + 1
+var result = document.getElementById('result');
+var resultTxt = document.getElementById('resultTxt');
 
-        while (elevate >= 1) {
-            for (let j = 0; j < numbers.length; j++) {
-                numA += numbers[j] * (i ** (elevate))
-                numB += numbers[j] * (k ** (elevate))
+document.getElementById("limpar").addEventListener('click', function(){
+    result.innerHTML = "";
+    resultTxt.innerHTML = "";
+});
 
-                if (numA < 0 && numB > 0) {
-                    intervals.push([i, k])
 
-                    if (i < j) {
-                        intervalsDefault.push([i, k])
-                    }
+document.getElementById("calcular").addEventListener('click', function(){
 
-                    if (i > j) {
-                        intervalsDefault.push([j, k])
-                    }
+    var type = document.getElementsByName("type");
+
+    casas = parseInt(document.getElementById("casas").value);
+    
+    var expressao = document.getElementById("expressao").value;
+
+    var xA = parseFloat(document.getElementById("a").value);
+    var xB = parseFloat(document.getElementById("b").value);
+
+    var precisao = parseFloat(document.getElementById("precisao").value);
+
+    var fxReal = parseInt(document.getElementById("fxReal").value);
+
+    var execucao = 1;
+    var maiorErro = true;
+
+    result.innerHTML = "";
+
+    var resultado = "";
+
+    do {
+            var fXA = roundValue(exeExpressao(expressao, xA));
+            var fXB = roundValue(exeExpressao(expressao, xB));
+
+            if(type[0].checked == true){
+                // Bissecção
+                var xN = roundValue((xB + xA) / 2);
+            } else{
+                // Falsa Posição
+                var xN = roundValue((xA*fXB - xB*fXA) / (fXB - fXA));
+            }
+
+            var fXN = roundValue(exeExpressao(expressao, xN));
+
+            if(type[0].checked == true){
+                // Bissecção
+                if(fXN == fxReal || (xB - xA) < precisao){
+                    maiorErro = false;
+                    resultado = "<hr/><p><strong>" +
+                                "Resultado: " + xN + "<br/>" +
+                                "Erro: " + (xB - xA) + "<br/>" +
+                                "Iterações: " + execucao + "<br/>" +
+                                "</strong></p>";
+    
+                    resultTxt.innerHTML = resultado;
                 }
-
-                if (numA > 0 && numB < 0) {
-                    intervals.push([k, i])
-
-                    if (i < j) {
-                        intervalsDefault.push([i, k])
-                    }
-
-                    if (i > j) {
-                        intervalsDefault.push([j, k])
-                    }
+            } else{
+                // Falsa Posição
+                if(fXN == fxReal || Math.abs(fXN) < precisao){
+                    maiorErro = false;
+                    resultado = "<hr/><p><strong>" +
+                                "Resultado: " + xN + "<br/>" +
+                                "Erro: " + (xB - xA) + "<br/>" +
+                                "Iterações: " + execucao + "<br/>" +
+                                "</strong></p>";
+    
+                    resultTxt.innerHTML = resultado;
                 }
-
-                elevate--
             }
-        }
-    }
-}
+        
+            var linha = "<tr><td>" + execucao + "</td><td>" +
+                    xA + "</td><td>" + 
+                    fXA + "</td><td>" +
+                    xB + "</td><td>" + 
+                    fXB + "</td><td>" + 
+                    xN + "</td><td>" +
+                    fXN + "</td><td>" + 
+                    roundValue((xB - xA)) + "</td></tr>";
 
-function calculateRoots() {
-    roots = []
+            result.innerHTML += linha;
+        
+            xA = ((fXA > fxReal && fXN > fxReal) || (fXA < fxReal && fXN < fxReal)) ? xN : xA;
+            xB = ((fXB > fxReal && fXN > fxReal) || (fXB < fxReal && fXN < fxReal)) ? xN : xB;  
+            
+            execucao++;
+        
+        } while(maiorErro);
 
-    for (let i = 0; i < intervals.length; i++) {
-        while (stopCriteria > precision) {
-            let result = 0
-            let elevate = order
-
-            let average = (intervals[i][0] + intervals[i][1]) / 2
-
-            for (let j = 0; j < numbers.length; j++) {
-                result += numbers[j] * (average ** (elevate))
-
-                elevate--
-            }
-
-            if (result > 0) {
-                intervals[i][1] = average
-            }
-
-            if (result < 0) {
-                intervals[i][0] = average
-            }
-
-            stopCriteria = Math.abs(intervals[i][0] - intervals[i][1])
-        }
-
-        roots.push([(intervals[i][0] + intervals[i][1]) / 2])
-
-        stopCriteria = 1
-    }
-}
-
-function showResults() {
-    let showStrongRoots = document.getElementById("strongRoots")
-    let showStrongIntervals = document.getElementById("strongIntervals")
-    let showStrongPrecision = document.getElementById("strongPrecision")
-
-    showStrongRoots.innerHTML = "Raízes"
-    showStrongIntervals.innerHTML = "Intervalos"
-    showStrongPrecision.innerHTML = "Erro"
-
-    let showResultRoots = document.getElementById("resultRoots")
-    let showResultIntervals = document.getElementById("resultIntervals")
-    let showResultPrecision = document.getElementById("resultPrecision")
-
-    let resultRoots = ""
-    let resultIntervals = ""
-
-    for (let i = 0; i < roots.length; i++) {
-        resultRoots += `<li>Raíz ${i + 1}:  ${roots[i]}</li> <br />`
-    }
-
-    for (let i = 0; i < intervalsDefault.length; i++) {
-        resultIntervals += `<li>Intervalo ${i + 1}: [${intervalsDefault[i][0]}, ${intervalsDefault[i][1]}]</li> <br />`
-    }
-
-    showResultRoots.innerHTML = resultRoots
-    showResultIntervals.innerHTML = resultIntervals
-    showResultPrecision.innerHTML = `<li>Erro menor que ${precision}</li>`
-}
-
-function calculateAll() {
-    getNumbers()
-    getPrecision()
-    calculateIntervals()
-    calculateRoots()
-    showResults()
-    drawFunction()
-}
-
-function addInputs() {
-    order = 0
-
-    order = document.getElementById("getOrder").value;
-    functionOrder = order
-    let numInputContainer = document.getElementById("numInputContainer");
-
-    while (numInputContainer.hasChildNodes()) {
-        numInputContainer.removeChild(numInputContainer.lastChild);
-    }
-
-    for (let i = 0; i <= order; i++) {
-        let letter = String.fromCharCode(i + 65)
-
-        const divElement = document.createElement("div")
-
-        const labelElement = document.createElement("label")
-
-        divElement.appendChild(labelElement)
-
-        labelElement.innerHTML = "Número " + letter
-
-        let numInput = document.createElement("input");
-
-        numInput.type = "number";
-        numInput.name = "number" + letter;
-        numInput.id = letter
-
-        divElement.appendChild(numInput);
-        numInputContainer.appendChild(divElement)
-
-    }
-}
-
-function drawFunction() {
-    for (let i = 0; i < numbers.length - 1; i++) {
-        if (numbers[i + 1] >= 0) {
-            expression += `${numbers[i]}*x^${functionOrder} +`
-        }
-
-        if (numbers[i + 1] < 0) {
-            expression += `${numbers[i]}*x^${functionOrder}`
-        }
-
-        functionOrder--
-    }
-
-    expression += `${numbers[numbers.length - 1]}`
-
-    let contentsBounds = document.body.getBoundingClientRect();
-    let width = 800;
-    let height = 500;
-    let ratio = contentsBounds.width / width;
-    width *= ratio;
-    height *= ratio;
-
-    function Plot() {
-        const text = {
-            target: "#root",
-            disableZoom: false,
-            width: 800,
-            height: 600,
-            yAxis: { domain: [-10, 10] },
-            grid: true,
-            data: [
-                {
-                    fn: expression,
-                }
-            ]
-        }
-    }
-
-}
-
-document.getElementById("generateInputs").addEventListener('click', addInputs)
-document.getElementById("calculateAll").addEventListener('click', calculateAll)
+});
